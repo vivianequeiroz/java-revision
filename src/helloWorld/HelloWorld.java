020c8284d82770074039a86235e8f7267393b166
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-26 20:50:58 -0300

    chore: the basic anatomy of a java program

---
+++ src/helloWorld/HelloWorld.java
@@ -0,0 +1,18 @@
+//Basic anatomy of a java program
+
+// package organize classes 
+// PascalCase is used for classes names
+package helloWorld;
+
+//classes organize methods 
+public class HelloWorld {
+	public static void main(String[] args) {
+		// inside a class it is called a method, but out of a class it is called a function
+		// camelCase is used for methods names
+		// the main method is from the program starts its execution
+	}
+}
+
+// the bytecode is what will be translated into the language that the SO the program is running and it is stored at the .class of the program 
+// javac HelloWorld.java + ls at the project package folder in the terminal will display the HelloWorld.java and HelloWorld.class files
+
