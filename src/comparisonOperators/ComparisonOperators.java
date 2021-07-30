fec4fd43b20aabf6b52be04963d58237ed7857cd
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-30 13:34:02 -0300

    chore: comparison operators - primitive types

---
+++ src/comparisonOperators/ComparisonOperators.java
@@ -0,0 +1,17 @@
+package comparisonOperators;
+
+public class ComparisonOperators {
+	
+	public static void main(String[] args) {
+		// Used for primitive types
+		int x = 1;
+		int y = 1;
+		
+		System.out.println(x == y);
+		System.out.println(x != y);
+		System.out.println(x >= y);
+		System.out.println(x <= y);
+		System.out.println(x > y);
+		System.out.println(x < y);
+	}
+}
