fe89f6e208fa8917bf71f0e34889b5779f495c7c
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-30 20:25:22 -0300

    chore: ternary operator

---
+++ src/TernaryOperator/TernaryOperator.java
@@ -0,0 +1,13 @@
+package TernaryOperator;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		int income = 120_000;
+		// Since its not a boolean variable, ternary operator is useful to validate an expression and the 
+		// assign a valid value to the variable 
+		String className = income > 100_000 ? "First" : "Economic";
+
+	}
+
+}
