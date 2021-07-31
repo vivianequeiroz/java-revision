cc7202fc59c3052f2686fa8fb81919fc42cea8bc
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-31 14:49:00 -0300

    chore: for each loops and its limitations and benefits

---
+++ src/forEachLoops/ForEachLoops.java
@@ -0,0 +1,18 @@
+package forEachLoops;
+
+public class ForEachLoops {
+
+	public static void main(String[] args) {
+		// Used to iterate over arrays or collections 
+		
+		String[] fruits = { "Apple", "Grape", "Strawberry" };
+
+		//the variable type of loops is the same of the array/collection
+		//Limitations: an array can't be iterated from the end to the beginning 
+					// the index of the variable created can't be accessable
+		for (String fruit : fruits) {
+			System.out.println(fruit);
+		}
+	}
+
+}
